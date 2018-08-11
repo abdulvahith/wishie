849df6bcce5bd9df62f86afbc13030df8de855c9
abdulvahith <abdul.vahith126@gmail.com>
2018-08-12 03:10:04 +0530

    login page validation comp

--- public/index.js
+++ public/index.js
@@ -28,32 +28,118 @@ signupBtn.addEventListener('click', (e) => {
 });
 
 $("#signUp").click(function() {
+	// $("#signUp").prop('disabled', true);
 	var uName = $("#s-name").val();
-	var uMail = $("#s-mail").val();
+	var uMail = isEmail($("#s-mail").val())?$("#s-mail").val():"";
 	var uPasswd = $("#s-passwd").val();
 	var uGender = $("#s-gender").val();
+	if(uName.length == 0){
+		toastr.error("Please Enter Username");
+		$("#s-name").focus();
+		return ;
+	}
+	else if(uMail.length == 0){
+		toastr.error("Please Enter Valid Mail Id");
+		$("#s-mail").focus();
+		return ;
+	}
+	else if(uGender == "select"){
+		toastr.error("Please select your gender");
+		$("#s-gender").focus();
+		return ;
+	}
+	else if(uPasswd.length == 0){
+		toastr.error("Please Enter Password");
+		$("#s-passwd").focus();
+		return ;
+	}
 	var signUpDtl = {
 		signUp_name : uName,
 		signUp_mail : uMail,
 		signUp_passwd : uPasswd,
 		signUp_gender : uGender
 	};
-	console.log(signUpDtl);
+	// console.log(signUpDtl);
+	$.blockUI({
+          message: 'Please Wait.....',
+          css: {
+            border: 'none',
+            padding: '15px',
+            backgroundColor: '#000',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            opacity: '.5',
+            color: '#fff',
+          	fontSize: '18px',
+            fontFamily: 'Verdana,Arial',
+          	fontWeight: 200,
+        } });
+	$.post( "/signupDetail", signUpDtl , function( data ) {
+		if (data == "fail")
+			toastr.error("Mail Id Aldready Registered Please Login");
+		else
+			toastr.success("Signup Successfully Please Login");
+
+		$("#login").trigger("click");
+			$.unblockUI();
+	});
 	$("#s-name,#s-mail,#s-passwd").val("");
 	$("#s-gender").val("select");
-	alert("clickd")
+	// $("#signUp").prop('disabled', false);
 })
 
 $("#logIn").click(function() {
-	var lMail = $("#l-mail").val();
+	// $("#logIn").prop('disabled', true);
+	var lMail = isEmail($("#l-mail").val())?$("#l-mail").val():"";
 	var lPasswd = $("#l-passwd").val();
+	if(lMail.length == 0){
+		toastr.error("Please Enter Valid Mail ID");
+		$("#l-mail").focus();
+		return ;
+	}
+	else if(lPasswd.length == 0){
+		toastr.error("Please Enter Password");
+		$("#l-passwd").focus();
+		return ;
+	}
 	var logDtl = {
 		log_mail : lMail,
 		log_passwd : lPasswd
 	}
+	$.blockUI({
+          message: 'Loading.......',
+          css: {
+            border: 'none',
+            padding: '15px',
+            backgroundColor: '#000',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            opacity: '.5',
+            color: '#fff',
+          	fontSize: '18px',
+            fontFamily: 'Verdana,Arial',
+          	fontWeight: 200,
+        } });
+	$.post("/userLogin", logDtl , function( data ) {
+		console.log(data);
+		$.unblockUI();
+		if (data == "mail not found")
+			toastr.error("Your Mail Id Not Registered");
+		else if(data == "pass failed")
+			toastr.error("Incorrect Password");
+		else{
+			$("#l-mail,#l-passwd").val("");
+			 window.location.href = '../module/user-form';
+		}
+	});
 	console.log(logDtl);
-	$("#l-mail,#l-passwd").val("");
-	alert("clickd23")
-	window.location.href = '../module/user-form';
+	// $("#logIn").prop('disabled', false);
+	// toastr.success("Login Successfully");
 	// window.location.replace("");
 })
+
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var result = regex.test(email);
+	return result;
+}
--- server.js
+++ server.js
@@ -9,23 +9,71 @@ const assert = require('assert');
 var url = "mongodb://abdul_vahi:Vahi123%24@cluster0-shard-00-00-nuxza.mongodb.net:27017,cluster0-shard-00-01-nuxza.mongodb.net:27017,cluster0-shard-00-02-nuxza.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 
 // Database Name
-const dbName = 'techcafe';
+const dbName = 'wishie';
 
-var bodyParser = require('body-parser')
-// app.use( bodyParser.json() );       // to support JSON-encoded bodies
-// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//   extended: true
-// }));
-// app.use(express.json());       // to support JSON-encoded bodies
-// app.use(express.urlencoded());
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get("/", function (request, response) {
   console.log(__dirname);
   response.sendFile(__dirname + '/index.html');
 });
 
+app.post('/signupDetail', function(req, res) {
+    var body = req.body;
+    console.log(req.body);
+    MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
+       if (err) throw err;
+        var dbo = db.db(dbName);
+        var existUser=[];
+        dbo.collection("wishData").find({ signUp_mail:body.signUp_mail }).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result.length)
+          existUser= result;
+           if(result.length> 0){
+             res.end("fail");
+           }
+          else{
+              var myobj = req.body;
+              dbo.collection("wishData").insertOne(myobj, function(err, res1) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                res.end("success");
+              });
+            }
+          db.close();
+        });
+  });
+});
+
+app.post('/userLogin', function(req, res) {
+    var body = req.body;
+    console.log(req.body);
+    MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
+       if (err) throw err;
+        var dbo = db.db(dbName);
+        var existUser=[];
+        dbo.collection("wishData").find({ signUp_mail:body.log_mail }).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result.length)
+          existUser= result;
+          console.log(result);
+          if(result.length == 0)
+            res.end("mail not found");
+          else if(result[0].signUp_passwd != body.log_passwd)
+            res.end("pass failed");
+          else
+            res.end("success");
+          db.close();
+        });
+  });
+});
 app.get("/userform", function (request, response) {
   console.log(__dirname);
   response.sendFile(__dirname + '/module/user-form/index.html');
